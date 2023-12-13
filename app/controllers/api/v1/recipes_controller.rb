7157b73f27202942b89540849ac8aceec8f6e74b
UtescherIntrieri <18ianaui1999@gmail.com>
2023-12-13 11:11:05 -0300

    API - Mostrando informações pedidas

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,6 +1,10 @@
 class Api::V1::RecipesController < ApplicationController
   def show
     recipe = Recipe.find(params[:id])
-    render status: 200, json: recipe.as_json
+    user = User.where(id: recipe.user_id)
+    user_email = user[0]["email"]
+    @final_view = [recipe, "user_email" => user_email]
+
+    render status: 200, json: @final_view.as_json(except: [:id, :recipe_type_id, :created_at, :updated_at, :user_id, :role])
   end
 end
